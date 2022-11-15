5c6ee00c8669e4597a2f8f7782f78f43b875ef5c
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-15 08:57:09 +0100

    add transaction create and new

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,38 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = Transaction.all
+    @group = current_user.groups.find(params[:group_id])
+    # @group = current_user.groups.find(params[:category_id])
+    @transactions = @group.ordered_transaction
+  end
+
+  def new
+    @category = current_user.ordered_groups
+    operation = Operation.new
+    id = params[:category_id]
+    respond_to do |format|
+      format.html { render :new, locals: { operation: operation, id: id } }
+    end
+  end
+
+  def create
+    operation = Operation.new(operation_params)
+    operation.user = current_user
+    respond_to do |format|
+      format.html do
+        if operation.save
+          flash[:notice] = 'Transaction was successfully created.'
+          redirect_to transactions_path
+        else
+          flash[:alert] = 'Transaction was not created.'
+          render :new, locals: { operation: operation }
+        end
+      end
+    end
+  end
+
+  private
+
+  def operation_params
+    params.require(:operation).permit(:amount, :group_id, :date)
   end
 end
