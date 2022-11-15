84226906450bcde6cf71a9d8c96a4b3d24323605
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-15 20:19:01 +0100

    fixed rubocop erro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
 
-  # def index
-  #   redirect_to new_user_registration_path
-  # end
+  def index
+    redirect_to new_user_registration_path
+  end
 end
