a56604a263c26d0c595944515c5fbf0c3e9ba04b
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-23 08:22:51 +0100

    made some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password name])
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
+  # # Catch all CanCan errors and alert the user of the exception
+  def after_sign_in_path_for(_resource)
+    categories_path
   end
 end
